3c251d661e3bf9e66a7926fb7f9c4eec23b1c389
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-09-17 22:21:36 -0700

    Refactors some functions into utility category

--- chess-move-check.py
+++ chess-move-check.py
@@ -1,6 +1,39 @@
-BOARD_SIZE = 8
+import sys
 
-# Parent class for all piece types
+### Constants ###
+BOARD_SIZE = 8
+COLORSET = set(['black', 'white']) # Used for checking if input color is valid
+
+
+### Utility Functions ###
+# Validates attribute with set. Returns normalized attribute on success.
+def validAttr(attr, checkSet):
+	normalAttr = attr.lower()
+
+	if normalAttr in checkSet:
+		return normalAttr
+
+	raise Exception('You input an invalid color or piece name!')
+
+# Converts input position from chess format (ex. B:3) to list index format (ex. 1,2)
+def formatIndex(position):
+	splitPos = position.split(':')
+	charCoord = splitPos[0].capitalize()
+
+	if charCoord not in Piece._CHARMAP:
+		raise Exception('You input a piece with an invalid X coordinate!')
+
+	if int(splitPos[1]) < 1 or int(splitPos[1]) > BOARD_SIZE:
+		raise Exception('You input a piece with an invalid Y coordinate!')
+
+	return {'x': Piece._CHARMAP[charCoord], 'y': int(splitPos[1]) - 1}
+
+# Converts input position from list index format (ex. 1,2) to chess format (ex. B:3)
+def formatChess(position):
+	return {'x': Piece._INDEXMAP[position['x']], 'y': str(position['y'] + 1)}
+
+
+### Parent class for all piece types ###
 class Piece:
 	# Used for converting from chess to index format
 	_CHARMAP = {
@@ -14,9 +47,6 @@ class Piece:
 		'H': 7
 	}
 
-	# Used for checking if input color is valid
-	_COLORSET = set(['black', 'white'])
-
 	# Used for converting from index to chess format
 	_INDEXMAP = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
@@ -25,18 +55,9 @@ class Piece:
 
 	# Constructor
 	def __init__(self, color, name, position):
-		self.color = self._validAttr(color, Piece._COLORSET)
-		self.name = self._validAttr(name, Piece._NAMESET)
-		self.position = self.indexFormat(position)
-
-	# Validates attribute with set. Returns normalized attribute on success.
-	def _validAttr(self, attr, checkSet):
-		normalAttr = attr.lower()
-
-		if normalAttr in checkSet:
-			return normalAttr
-
-		raise Exception('You input an invalid color or piece name!')
+		self.color = validAttr(color, COLORSET)
+		self.name = validAttr(name, Piece._NAMESET)
+		self.position = formatIndex(position)
 
 	# Implementation for move validation. Changes depending on piece type.
 	def _moveCalc(self, board):
@@ -46,46 +67,41 @@ class Piece:
 	def moveCheck(self, board):
 		return self.moveCalc(board)
 
-	# Converts input position from chess format (ex. B:3) to list index format (ex. 1,2)
-	def indexFormat(self, position):
-		splitPos = position.split(':')
-		charCoord = splitPos[0].capitalize()
-
-		if charCoord not in Piece._CHARMAP:
-			raise Exception('You input a piece with an invalid X coordinate!')
-
-		if int(splitPos[1]) < 1 or int(splitPos[1]) > BOARD_SIZE:
-			raise Exception('You input a piece with an invalid Y coordinate!')
-
-		return {'x': Piece._CHARMAP[charCoord], 'y': int(splitPos[1]) - 1}
-
-	# Converts input position from list index format (ex. 1,2) to chess format (ex. B:3)
-	def chessFormat(self, position):
-		return {'x': Piece._INDEXMAP[position['x']], 'y': str(position['y'] + 1)}
-
 	# Prints out piece info (Color, Name, Position)
 	def printPiece(self):
 		print self.color.capitalize() + ', ' + self.name.capitalize() + ', <' + str(self.position['x']) + ':' + str(self.position['y']) + '>'
 
+
+### Chessboard Class ###
 class Chessboard:
-	def __init__(self, *args):
-		# Initialize board
-		self.board = []
-		self.pieces = []
+	def __init__(self, playerColor, *args):
+		self.playerColor = validAttr(playerColor, COLORSET) # Set player color to determine who's turn it is
+		self.pieces = [] # List of all pieces
+		self.board = [] # 2D array representation of board
 
-		for arg in args[1:]:
-			print arg
-
+		# Populate pieces
+		for arg in args:
+			if type(arg) is not list or len(arg) != 2:
+				raise Exception('You input an invalid piece!')
+			else:
+				print 'test'
+	
+		# Initialize empty board
 		for x in range(BOARD_SIZE):
 			self.board.append([])
 			for y in range(BOARD_SIZE):
 				self.board[x].append(None)
 
-# main()
-chessboard = Chessboard('var', 'meh', 'urmum')
+
+### main() ###
+chessboard = Chessboard(sys.argv[1], sys.argv[2:])
+
+#open(sys.argv[1])
 
 piece = Piece('bLaCk', 'KiNg', 'H:8')
 piece.printPiece()
 
-chessPos = piece.chessFormat(piece.position)
+chessPos = formatChess(piece.position)
 print '<' + chessPos['x'] + ':' + chessPos['y'] + '>'
+
+print len(sys.argv)
